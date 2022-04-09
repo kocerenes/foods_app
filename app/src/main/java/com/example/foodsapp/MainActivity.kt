bcf22e7fe2e05e2b60173f04fafff70cae747dcb
kocerenes <eneskocerr41@gmail.com>
2022-04-09 22:59:45 +0300

    Create topBar.

--- app/src/main/java/com/example/foodsapp/MainActivity.kt
+++ app/src/main/java/com/example/foodsapp/MainActivity.kt
@@ -3,10 +3,9 @@ package com.example.foodsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.foodsapp.ui.theme.FoodsAppTheme
 
@@ -17,7 +16,7 @@ class MainActivity : ComponentActivity() {
             FoodsAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    HomePage()
                 }
             }
         }
@@ -25,14 +24,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomePage() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {Text(text = "Yemekler")},
+                backgroundColor = colorResource(id = R.color.essentialColor),
+                contentColor = colorResource(id = R.color.white)
+            )
+        },
+        content = {
+
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FoodsAppTheme {
-        Greeting("Android")
+        HomePage()
     }
 }
